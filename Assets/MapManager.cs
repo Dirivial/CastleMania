2585075e898c7406cf9c7b5078450d43094bebfb
Dirivial <alexander.kadeby@gmail.com>
2025-03-03 20:08:36 +0100

    New idea for map generation, more continuous

--- Assets/MapManager.cs
+++ Assets/MapManager.cs
@@ -2,19 +2,157 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct Room
+{
+    public int id;
+    public string name;
+}
+
 public class MapManager : MonoBehaviour
 {
-    private Dictionary<Vector2Int, bool> map = new Dictionary<Vector2Int, bool>();
 
-    // Start is called before the first frame update
-    void Start()
+    [Header("Map Generation Settings")]
+    public int numArenas = 10; // Total number of arenas (including start and boss arenas)
+    public int minSideRooms = 2; // Minimum number of side rooms
+    public int maxSideRooms = 5; // Maximum number of side rooms
+
+    public List<List<Vector2Int>> roomTypes = new List<List<Vector2Int>>();
+
+    private Dictionary<Vector2Int, Room> map = new Dictionary<Vector2Int, Room>();
+
+    public Dictionary<Vector2Int, Room> GenerateMap()
     {
-        
+        // Generate the main path using DFS
+        GenerateMainPath();
+
+        // Add side rooms
+        //AddSideRooms();
+
+        PrintMap();
+
+        return map;
     }
 
-    // Update is called once per frame
-    void Update()
+    void GenerateMainPath()
     {
-        
+        HashSet<Vector2Int> visited = new HashSet<Vector2Int>();
+
+        // Start DFS from the start arena (0)
+        Vector2Int startPos = new Vector2Int(0, 0);
+        DFS(0, startPos, visited);
     }
+
+    bool DFS(int depth, Vector2Int currentPos, HashSet<Vector2Int> visited)
+    {
+        visited.Add(currentPos);
+
+        // Abort if we are next to two paths
+        int numAdjacent = 0;
+        for (int i = -1; i < 2; i += 2)
+        {
+            if (visited.Contains(new Vector2Int(currentPos.x + i, currentPos.y)))
+            {
+                numAdjacent++;
+            }
+            if (visited.Contains(new Vector2Int(currentPos.x, currentPos.y + i)))
+            {
+                numAdjacent++;
+            }
+        }
+        if (numAdjacent > 1)
+        {
+            return false;
+        }
+
+
+        // Stop if we've reached the boss arena
+        if (depth == numArenas - 1)
+        {
+            Room r = new Room { id = 1, name = "Test" };
+            map.Add(currentPos, r);
+            return true;
+        }
+
+        // Randomly choose the next arena in the main path
+        List<int> directions = new List<int> { 0, 1, 2, 3 };
+        for (int i = 0; i < 4; i++)
+        {
+            // Choose a direction randomly 
+            int index = Random.Range(0, 4 - i);
+            int dir = directions[index];
+            directions.Remove(index);
+
+            Vector2Int nextPos = new Vector2Int(currentPos.x, currentPos.y);
+
+            switch (dir)
+            {
+                case 0:
+                    // Up
+                    nextPos.y += 1;
+                    break;
+                case 1:
+                    // Right
+                    nextPos.x += 1;
+                    break;
+                case 2:
+                    // Down 
+                    nextPos.y -= 1;
+                    break;
+                case 3:
+                    // Left
+                    nextPos.x -= 1;
+                    break;
+            }
+
+            if (!visited.Contains(nextPos))
+            {
+                bool ret = DFS(depth + 1, nextPos, visited);
+                if (ret)
+                {
+                    Room r = new Room { id = 1, name = "Test" };
+                    map.Add(currentPos, r);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    void AddSideRooms(List<int> mainPath)
+    {
+        int numSideRooms = UnityEngine.Random.Range(minSideRooms, maxSideRooms + 1);
+
+        for (int i = 0; i < numSideRooms; i++)
+        {
+            // Choose a random arena on the main path to attach a side room
+            int mainPathArena = mainPath[UnityEngine.Random.Range(0, mainPath.Count - 1)];
+
+            // Find an unused arena to use as a side room
+            int sideRoom = UnityEngine.Random.Range(1, numArenas);
+            while (mainPath.Contains(sideRoom)) // Ensure the side room isn't on the main path
+            {
+                sideRoom = UnityEngine.Random.Range(1, numArenas);
+            }
+
+            // Connect the main path arena to the side room
+            //map[mainPathArena].Add(sideRoom);
+
+            // Optionally, connect the side room back to the main path (for loops)
+            if (UnityEngine.Random.value < 0.5f) // 50% chance to create a loop
+            {
+                //map[sideRoom].Add(mainPathArena);
+            }
+        }
+    }
+
+    void PrintMap()
+    {
+        string path = "";
+        foreach (var key in map.Keys)
+        {
+            path += " -> " + key.ToString();
+        }
+        Debug.Log(path);
+    }
+
 }
--- Assets/Scripts/ChunkManager.cs
+++ Assets/Scripts/ChunkManager.cs
@@ -1,11 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public struct Arena
-{
-    int x;
-    int y;
-}
 
 public class ChunkManager : MonoBehaviour
 {
@@ -17,18 +12,13 @@ public class ChunkManager : MonoBehaviour
 
     public int distanceBetweenChunks = 25;
 
-    [Header("Map Generation Settings")]
-    public int numArenas = 10; // Total number of arenas (including start and boss arenas)
-    public int minSideRooms = 2; // Minimum number of side rooms
-    public int maxSideRooms = 5; // Maximum number of side rooms
 
     private WFCManager wfcManager;
     private LavaManager lavaManager;
+    private MapManager mapManager;
     private Vector2Int currentPlayerChunk;
     private Dictionary<Vector2Int, bool> chunks = new Dictionary<Vector2Int, bool>();
-    private Dictionary<int, List<int>> arenaMap = new Dictionary<int, List<int>>();
-
-    private List<Vector2Int> arenas = new List<Vector2Int>();
+    private Dictionary<Vector2Int, Room> map = new Dictionary<Vector2Int, Room>();
 
     public void Awake()
     {
@@ -37,13 +27,13 @@ public class ChunkManager : MonoBehaviour
         wfcManager.Setup();
 
         lavaManager = GetComponent<LavaManager>();
+        mapManager = GetComponent<MapManager>();
     }
 
     private void Start()
     {
         // Create map layout
-        GenerateMap();
-        PrintMap();
+        map = mapManager.GenerateMap();
 
         // Spawn chunks close to the player
         UpdateChunks();
@@ -81,7 +71,7 @@ public class ChunkManager : MonoBehaviour
                 Vector2Int chunkPos = new Vector2Int(x, y);
 
                 // Check if the chunk is already loaded
-                if (arenas.Contains(chunkPos) && !chunks.ContainsKey(chunkPos))
+                if (map.ContainsKey(chunkPos) && !chunks.ContainsKey(chunkPos))
                 {
                     chunks.Add(chunkPos, true);
                     wfcManager.CreateChunk(chunkPos);
@@ -109,132 +99,4 @@ public class ChunkManager : MonoBehaviour
         }
     }
 
-    void GenerateMap()
-    {
-        // Generate the main path using DFS
-        GenerateMainPath();
-
-        // Add side rooms
-        //AddSideRooms();
-    }
-
-    void GenerateMainPath()
-    {
-        HashSet<Vector2Int> visited = new HashSet<Vector2Int>();
-
-        // Start DFS from the start arena (0)
-        Vector2Int startPos = new Vector2Int(0, 0);
-        DFS(0, startPos, visited, arenas);
-    }
-
-    bool DFS(int depth, Vector2Int currentPos, HashSet<Vector2Int> visited, List<Vector2Int> mainPath)
-    {
-        visited.Add(currentPos);
-
-        // Abort if we are next to two paths
-        int numAdjacent = 0;
-        for (int i = -1; i < 2; i += 2)
-        {
-            if (visited.Contains(new Vector2Int(currentPos.x + i, currentPos.y)))
-            {
-                numAdjacent++;
-            }
-            if (visited.Contains(new Vector2Int(currentPos.x, currentPos.y + i)))
-            {
-                numAdjacent++;
-            }
-        }
-        if (numAdjacent > 1)
-        {
-            return false;
-        }
-
-
-        // Stop if we've reached the boss arena
-        if (depth == numArenas - 1)
-        {
-            mainPath.Add(currentPos);
-            return true;
-        }
-
-        // Randomly choose the next arena in the main path
-        List<int> directions = new List<int> { 0, 1, 2, 3 };
-        for (int i = 0; i < 4; i++)
-        {
-            // Choose a direction randomly 
-            int index = Random.Range(0, 4 - i);
-            int dir = directions[index];
-            directions.Remove(index);
-
-            Vector2Int nextPos = new Vector2Int(currentPos.x, currentPos.y);
-
-            switch (dir)
-            {
-                case 0:
-                    // Up
-                    nextPos.y += 1;
-                    break;
-                case 1:
-                    // Right
-                    nextPos.x += 1;
-                    break;
-                case 2:
-                    // Down 
-                    nextPos.y -= 1;
-                    break;
-                case 3:
-                    // Left
-                    nextPos.x -= 1;
-                    break;
-            }
-
-            if (!visited.Contains(nextPos))
-            {
-                bool ret = DFS(depth + 1, nextPos, visited, mainPath);
-                if (ret)
-                {
-                    mainPath.Add(currentPos);
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    void AddSideRooms(List<int> mainPath)
-    {
-        int numSideRooms = UnityEngine.Random.Range(minSideRooms, maxSideRooms + 1);
-
-        for (int i = 0; i < numSideRooms; i++)
-        {
-            // Choose a random arena on the main path to attach a side room
-            int mainPathArena = mainPath[UnityEngine.Random.Range(0, mainPath.Count - 1)];
-
-            // Find an unused arena to use as a side room
-            int sideRoom = UnityEngine.Random.Range(1, numArenas);
-            while (mainPath.Contains(sideRoom)) // Ensure the side room isn't on the main path
-            {
-                sideRoom = UnityEngine.Random.Range(1, numArenas);
-            }
-
-            // Connect the main path arena to the side room
-            arenaMap[mainPathArena].Add(sideRoom);
-
-            // Optionally, connect the side room back to the main path (for loops)
-            if (UnityEngine.Random.value < 0.5f) // 50% chance to create a loop
-            {
-                arenaMap[sideRoom].Add(mainPathArena);
-            }
-        }
-    }
-
-    void PrintMap()
-    {
-        string path = arenas[arenas.Count - 1].ToString();
-        for (int i = arenas.Count - 2; i >= 0; i--)
-        {
-            path += " -> " + arenas[i].ToString();
-        }
-        Debug.Log(path);
-    }
 }
